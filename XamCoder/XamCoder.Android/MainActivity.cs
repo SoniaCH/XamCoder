e0d88a2f7d149ac85ff56a8bed45caa3c4a31684
SoniaChnini <chnini.sonia@gmail.com>
2018-10-17 11:06:45 +0100

    Add project files.

---
+++ XamCoder/XamCoder.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Util;
+
+namespace XamCoder.Droid
+{
+    [Activity(Label = "XamCoder", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            // this is for the height and wdth of the screen
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            WindowManager.DefaultDisplay.GetRealMetrics(displayMetrics);
+            App.ScreenHeight = (int)(Resources.DisplayMetrics.HeightPixels / Resources.DisplayMetrics.Density);
+            App.ScreenWidth = (int)(Resources.DisplayMetrics.WidthPixels / Resources.DisplayMetrics.Density);
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ XamCoder/XamCoder/Views/Menu.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamCoder
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : ContentPage
+	{
+        private double _raduis;
+        public double Raduis
+        {
+            get { return _raduis; }
+            set { _raduis = value;OnPropertyChanged(); }
+        }
+        private double _raduisTwo;
+        public double RaduisTwo
+        {
+            get { return _raduisTwo; }
+            set { _raduisTwo = value; OnPropertyChanged(); }
+        }
+        private double _smallRaduis;
+        public double SmallRaduis
+        {
+            get { return _smallRaduis; }
+            set { _smallRaduis = value; OnPropertyChanged(); }
+        }
+        private double _halfRaduis;
+        public double HalfRaduis
+        {
+            get { return _halfRaduis; }
+            set { _halfRaduis = value; OnPropertyChanged(); }
+        }
+        private Thickness _padding;
+        public Thickness Padding
+        {
+            get { return _padding; }
+            set { _padding = value; OnPropertyChanged(); }
+        }
+
+        public Menu ()
+		{
+			InitializeComponent ();
+            BindingContext = this;
+            SmallRaduis = App.ScreenWidth / 10;
+            Raduis = App.ScreenWidth - App.ScreenWidth / 5;
+            RaduisTwo = Raduis + App.ScreenWidth / 5;
+            HalfRaduis = RaduisTwo / 2;
+            Padding = new Thickness(-(RaduisTwo / 2),SmallRaduis,0,0);
+            box1.WidthRequest = HalfRaduis+ SmallRaduis;
+            box2.WidthRequest = HalfRaduis + 2.35 * SmallRaduis;
+            box3.WidthRequest = HalfRaduis + 3.35 * SmallRaduis;
+            box5.WidthRequest = App.ScreenWidth - SmallRaduis-2;
+            box7.WidthRequest = HalfRaduis + 3.35 * SmallRaduis; 
+            box8.WidthRequest = HalfRaduis + 2.35 * SmallRaduis; 
+            box9.WidthRequest = HalfRaduis + SmallRaduis; 
+        }
+	}
+}
